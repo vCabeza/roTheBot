1be326e59579f2eea1c5f148a7edb5ec950d82af
Vicente <vcabeza1993@gmail.com>
2020-07-28 18:39:48 +0200

    Updates

---
+++ precache-manifest.63c1966c3426b819166f38056979a246.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c15ecc5bd15fba8790c8c00e61856e77",
+    "url": "/roTheBot/index.html"
+  },
+  {
+    "revision": "c82cf8280af4429944e3",
+    "url": "/roTheBot/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "b9701524fb93e6e75832",
+    "url": "/roTheBot/static/css/main.ab502c3c.chunk.css"
+  },
+  {
+    "revision": "c82cf8280af4429944e3",
+    "url": "/roTheBot/static/js/2.7e2d2c3d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/roTheBot/static/js/2.7e2d2c3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9701524fb93e6e75832",
+    "url": "/roTheBot/static/js/main.7a5d7c57.chunk.js"
+  },
+  {
+    "revision": "1c044b96f05b4f5530e2",
+    "url": "/roTheBot/static/js/runtime-main.86a6ecc3.js"
+  }
+]);
